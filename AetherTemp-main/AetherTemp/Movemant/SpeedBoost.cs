1bb6384b65526ca9dfd84801989b0ee7d8cad2f6
Teuz1013 <matteusgordon45@gmail.com>
2025-03-15 20:27:52 +0100

    boys
    hi kids

---
+++ AetherTemp-main/AetherTemp/Movemant/SpeedBoost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Troka_Menu.Movment
+{
+    class SpeedBoost
+    {
+        public static void SpeedboostMod()
+        {
+            GorillaLocomotion.Player.Instance.maxJumpSpeed = 9f;
+            GorillaLocomotion.Player.Instance.jumpMultiplier = 9f;
+        }
+
+        internal static void SpeedBoostMod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AetherTemp-main/AetherTemp/Rig/UpSideDownHead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AetherTemp.Rig
+{
+    class UpSideDownHead
+    {
+        public static void UpsideDownHeadG()
+        {
+            if (ControllerInputPoller.instance.rightGrab)
+            {
+                GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.z = 180f;
+            }
+            if (ControllerInputPoller.instance.leftGrab)
+            {
+                GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.z = 180f;
+            }
+            else
+            {
+                GorillaTagger.Instance.offlineVRRig.head.trackingRotationOffset.z = 0f;
+            }
+        }
+    }
+}
